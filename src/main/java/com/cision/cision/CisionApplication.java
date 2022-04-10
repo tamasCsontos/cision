fc514da8ae60a59af235db7f531248298c891ffc
AzureAD\CsontosTamás <csonti515@gmail.com>
2022-04-10 23:04:10 +0200

    establish repo

---
+++ src/main/java/com/cision/cision/CisionApplication.java
@@ -0,0 +1,13 @@
+package com.cision.cision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cision/cision/CisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cision.cision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
